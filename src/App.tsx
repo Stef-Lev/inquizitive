fdcd71b2d1bf1653c6165029891a3549424d0a17
Stefanos Leventis <stefanos.leventis@upstreamsystems.com>
2021-02-09 17:06:30 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+export default App;
+//https://opentdb.com/api.php?amount=10&type=multiple
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QuestionCard = () => {
+    return (
+        <div>
+            QUESTION CARD
+        </div>
+    )
+}
+
+export default QuestionCard;
